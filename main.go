f135e8ea08201e6e9b70008a9d7581a8edd29f0e
Trump2022 <qsw2012@outlook.com>
2024-02-26 17:54:17 +0800

    test

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog </h1>")
+	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	fmt.Fprint(w, "请求路径为："+r.URL.Path)
 }
 
 func main() {
