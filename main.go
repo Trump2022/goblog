7693f548e3b1eff6c6acc3965aeb34ed66b09ad9
Trump2022 <qsw2012@outlook.com>
2024-02-27 14:50:30 +0800

    404 状态码

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
@@ -21,5 +21,5 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe("localhost:3000", nil)
+	http.ListenAndServe(":3000", nil)
 }
