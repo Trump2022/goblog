866408f68bbf056825e08208e90407d7bc95eba9
Trump2022 <qsw2012@outlook.com>
2025-04-24 10:27:26 +0800

    save

--- main.go
+++ main.go
@@ -6,9 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>请求路径为："+r.URL.Path+"</h1>")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
@@ -17,7 +16,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("localhost:3000", nil)
 }
