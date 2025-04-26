8146f38c331ded508755b15405ed8eb56ea40737
Trump2022 <qsw2012@outlook.com>
2025-04-26 16:09:36 +0800

    使用模板文件

--- main.go
+++ main.go
@@ -92,28 +92,28 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// fmt.Fprintf(w, "有错误发生, errors的值为: %v <br>", errors)
 
-		html := `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>创建文章 —— 我的技术博客</title>
-    <style type="text/css">.error {color: red;}</style>
-</head>
-<body>
-    <form action="{{ .URL }}" method="post">
-        <p><input type="text" name="title" value="{{ .Title }}"></p>
-        {{ with .Errors.title }}
-        <p class="error">{{ . }}</p>
-        {{ end }}
-        <p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
-        {{ with .Errors.body }}
-        <p class="error">{{ . }}</p>
-        {{ end }}
-        <p><button type="submit">提交</button></p>
-    </form>
-</body>
-</html>
-`
+		// 		html := `
+		// <!DOCTYPE html>
+		// <html lang="en">
+		// <head>
+		//     <title>创建文章 —— 我的技术博客</title>
+		//     <style type="text/css">.error {color: red;}</style>
+		// </head>
+		// <body>
+		//     <form action="{{ .URL }}" method="post">
+		//         <p><input type="text" name="title" value="{{ .Title }}"></p>
+		//         {{ with .Errors.title }}
+		//         <p class="error">{{ . }}</p>
+		//         {{ end }}
+		//         <p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+		//         {{ with .Errors.body }}
+		//         <p class="error">{{ . }}</p>
+		//         {{ end }}
+		//         <p><button type="submit">提交</button></p>
+		//     </form>
+		// </body>
+		// </html>
+		// `
 		storeURL, _ := router.Get("articles.store").URL()
 
 		data := ArticlesFormData{
@@ -122,7 +122,8 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 			URL:    storeURL,
 			Errors: errors,
 		}
-		tmpl, err := template.New("create-form").Parse(html)
+		// tmpl, err := template.New("create-form").Parse(html)
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
 		if err != nil {
 			panic(err)
 		}
@@ -156,23 +157,40 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>创建文章 —— 我的技术博客</title>
-</head>
-<body>
-	<form action="%s?test=data" method="post">
-        <p><input type="text" name="title"></p>
-        <p><textarea name="body" cols="30" rows="10"></textarea></p>
-        <p><button type="submit">提交</button></p>
-    </form>
-</body>
-</html>
-`
+	// 	html := `
+	// <!DOCTYPE html>
+	// <html lang="en">
+	// <head>
+	//     <title>创建文章 —— 我的技术博客</title>
+	// </head>
+	// <body>
+	// 	<form action="%s?test=data" method="post">
+	//         <p><input type="text" name="title"></p>
+	//         <p><textarea name="body" cols="30" rows="10"></textarea></p>
+	//         <p><button type="submit">提交</button></p>
+	//     </form>
+	// </body>
+	// </html>
+	// `
+	// storeURL, _ := router.Get("articles.store").URL()
+	// fmt.Fprintf(w, html, storeURL)
+
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+	data := ArticlesFormData{
+		Title:  "",
+		Body:   "",
+		URL:    storeURL,
+		Errors: nil,
+	}
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
 
 }
 
